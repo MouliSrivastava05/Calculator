567b4ca6b29b6a60d65bd54a94eaa6541edfebf1
MouliSrivastava05 <moulicode5@gmail.com>
2025-02-15 14:36:37 +0530

    commitcalc

---
+++ script.js
@@ -0,0 +1,20 @@
+function calc(value) {
+    document.getElementById("display").value += value;
+}
+
+function clearDisplay() {
+    document.getElementById("display").value = "";
+}
+
+function backspace() {
+    let display = document.getElementById("display");
+    display.value = display.value.slice(0, -1);
+}
+
+function calcResult() {
+    try {
+        document.getElementById("display").value = eval(document.getElementById("display").value);
+    } catch {
+        document.getElementById("display").value = "Error";
+    }
+}
